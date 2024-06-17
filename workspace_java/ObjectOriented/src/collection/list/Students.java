0010d7be970d43805a340aad4d0d048a8407ccf6
gyals8948 <gyals8948@gmail.com>
2024-06-17 12:51:37 +0900

    12

---
+++ workspace_java/ObjectOriented/src/collection/list/Students.java
@@ -0,0 +1,33 @@
+package collection.list;
+
+public class Students {
+
+    String name;
+    int age;
+    int point;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getPoint() {
+        return point;
+    }
+
+    public void setPoint(int point) {
+        this.point = point;
+    }
+}
+
